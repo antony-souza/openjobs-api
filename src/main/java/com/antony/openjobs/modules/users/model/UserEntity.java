6027c94bf3d3baec1af33d67989d72e255a60224
antony-souza <antonygustavo09@hotmail.com>
2026-09-01 22:28:25 -0300

    refactor: enhance UserEntity model with additional fields and annotations

--- src/main/java/com/antony/openjobs/modules/users/model/UserEntity.java
+++ src/main/java/com/antony/openjobs/modules/users/model/UserEntity.java
@@ -1,4 +1,46 @@
 package com.antony.openjobs.modules.users.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column()
+    private String code;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
 }
