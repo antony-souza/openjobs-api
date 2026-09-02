58d53be23b597e877472679754040f2a618e1775
antony-souza <antonygustavo09@hotmail.com>
2026-09-01 21:48:04 -0300

    test

---
+++ src/main/java/com/antony/openjobs/OpenjobsApplication.java
@@ -0,0 +1,13 @@
+package com.antony.openjobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenjobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenjobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antony/openjobs/OpenjobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antony.openjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenjobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
