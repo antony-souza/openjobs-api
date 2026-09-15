69b3afc9c73b3c1ee6315ea74c282d2b19cb116b
antony-souza <antonygustavo09@hotmail.com>
2026-09-14 23:09:59 -0300

    feat: implement update and delete job functionalities in JobController; add corresponding use cases and responses

--- src/main/java/com/antony/openjobs/modules/jobs/controller/JobController.java
+++ src/main/java/com/antony/openjobs/modules/jobs/controller/JobController.java
@@ -5,32 +5,63 @@ import com.antony.openjobs.config.security.AuthenticatedUser;
 import com.antony.openjobs.modules.jobs.usecase.create.CreateJobRequest;
 import com.antony.openjobs.modules.jobs.usecase.create.CreateJobResponse;
 import com.antony.openjobs.modules.jobs.usecase.create.CreateJobUseCase;
+import com.antony.openjobs.modules.jobs.usecase.delete.DeleteJobResponse;
+import com.antony.openjobs.modules.jobs.usecase.delete.DeleteJobUseCase;
+import com.antony.openjobs.modules.jobs.usecase.update.UpdateJobRequest;
+import com.antony.openjobs.modules.jobs.usecase.update.UpdateJobResponse;
+import com.antony.openjobs.modules.jobs.usecase.update.UpdateJobUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController()
 @RequestMapping("/v1/jobs")
 @RequiredArgsConstructor
 public class JobController {
     private final CreateJobUseCase createJobUseCase;
+    private final UpdateJobUseCase updateJobUseCaseJobUseCase;
+    private final DeleteJobUseCase deleteJobUseCase;
 
     @PostMapping()
     public ResponseEntity<ApiResponse<CreateJobResponse>> create(
-            @Valid
-            @RequestBody CreateJobRequest request,
+            @Valid @RequestBody CreateJobRequest request,
             @AuthenticationPrincipal AuthenticatedUser loggedUser
     ) {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(ApiResponse.success(
                         createJobUseCase.execute(request, loggedUser.userId())
+                ));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<UpdateJobResponse>> create(
+            @PathVariable("id") UUID jobId,
+            @Valid @RequestBody UpdateJobRequest request,
+            @AuthenticationPrincipal AuthenticatedUser loggedUser
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success(
+                        updateJobUseCaseJobUseCase.execute(jobId, request, loggedUser.userId())
+                ));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<DeleteJobResponse>> delete(
+            @PathVariable("id") UUID jobId,
+            @AuthenticationPrincipal AuthenticatedUser loggedUser
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(ApiResponse.success((
+                        deleteJobUseCase.execute(jobId, loggedUser.userId()))
                 ));
     }
 }
--- src/main/java/com/antony/openjobs/modules/jobs/usecase/delete/DeleteJobUseCase.java
+++ src/main/java/com/antony/openjobs/modules/jobs/usecase/delete/DeleteJobUseCase.java
@@ -1,9 +1,29 @@
 package com.antony.openjobs.modules.jobs.usecase.delete;
 
+import com.antony.openjobs.modules.jobs.model.JobEntity;
+import com.antony.openjobs.modules.jobs.repository.JobRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class DeleteJobUseCase {
+    private final JobRepository jobRepository;
+
+    @Transactional
+    public DeleteJobResponse execute(UUID jobId, UUID publishedById) {
+        JobEntity jobEntity = jobRepository
+                .findByIdAndPublishedBy_IdAndDeletedAtIsNull(jobId, publishedById)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Job not found"));
+
+        jobEntity.setDeletedAt(LocalDateTime.now());
+
+        return new DeleteJobResponse("Vaga apagada com sucesso");
+    }
 }
