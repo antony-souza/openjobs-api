e946011c4cee5316d2cd08863d42fdb78a8e1fce
antony-souza <antonygustavo09@hotmail.com>
2026-09-15 23:36:39 -0300

    feat: implement ApplicationRepository interface with methods for application data access

--- src/main/java/com/antony/openjobs/modules/applications/repository/ApplicationRepository.java
+++ src/main/java/com/antony/openjobs/modules/applications/repository/ApplicationRepository.java
@@ -1,4 +1,21 @@
 package com.antony.openjobs.modules.applications.repository;
 
-public interface ApplicationRepository {
+import com.antony.openjobs.modules.applications.model.ApplicationEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ApplicationRepository extends JpaRepository<ApplicationEntity, UUID> {
+
+    Optional<ApplicationEntity> findByIdAndDeletedAtIsNull(UUID id);
+
+    Page<ApplicationEntity> findByCandidate_IdAndCandidate_DeletedAtIsNullAndJob_DeletedAtIsNullAndDeletedAtIsNull(
+            UUID candidateId,
+            Pageable pageable
+    );
 }
