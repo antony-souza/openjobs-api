02156c9d62e10171d129cbb87b20cfb35fe3249f
antony-souza <antonygustavo09@hotmail.com>
2026-09-21 21:30:55 -0300

    feat: add FindAllJobsProjection and FindAllJobsUseCase for job retrieval

---
+++ src/main/java/com/antony/openjobs/modules/jobs/usecase/findall/FindAllJobsProjection.java
@@ -0,0 +1,4 @@
+package com.antony.openjobs.modules.jobs.usecase.findall;
+
+public record FindAllJobsProjection() {
+}
---
+++ src/main/java/com/antony/openjobs/modules/jobs/usecase/findall/FindAllJobsUseCase.java
@@ -0,0 +1,4 @@
+package com.antony.openjobs.modules.jobs.usecase.findall;
+
+public class FindAllJobsUseCase {
+}
